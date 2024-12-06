3d5dcbfb3963a4812df9f973a1bf884c6c6b60bc
Raayavegas <veeralarithika@gmail.com>
2024-12-06 12:44:36 +0530

    Add files via upload

---
+++ nestedfor4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                int k=(i+j);
+                System.out.print(k+" ");
+            }
+            System.out.println();
+        }
+    }
+}
